a760ac16816effec4d5aba4d6ebb58468563c687
Yi Peng <pengyi0318@g.ucla.edu>
2018-07-31 15:54:38 -0700

    pi time correct

--- Yi_Peng/pi.c
+++ Yi_Peng/pi.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <time.h>
 
 static int long numSteps = 100000;
-double pi_incr (double t);
+double pi_incr (double x);
 int main() {
+    clock_t t1, t2;
+    t1 = clock();
+    double pi = 0.0; double time = 0.0; double x = 0.0;
+    double x_incr = 1.0/numSteps;
+    printf("t_incr = %f \n", x_incr);
 
-    double pi = 0.0; double time = 0.0; 
-    double time_incr = 1.0/numSteps;
-    printf("time_incr = %f \n", time_incr);
     for (int i = 0; i<numSteps; i++) {
-        pi += pi_incr(time)*time_incr;
-        time += time_incr;        
+        pi += pi_incr(x)*x_incr;
+        x += x_incr;   
+    
     }
+
+    t2 = clock(); 
+    time = ((double)(t2-t1))/CLOCKS_PER_SEC;
     printf("PI = %f, duration: %f ms \n",pi,time);
     return 0;
 }
 
-double pi_incr (double t) {
-      double pi_incr = 4.0/(1+t*t);
+double pi_incr (double x) {
+      double pi_incr = 4.0/(1+x*x);
       return pi_incr;
     }
