08f656aac1e379ea911c5fa1cf1adf001d9a3c3c
fmckenna <fmckenna@berkeley.edu>
2018-07-30 21:52:31 -0700

    fmk - modification send2.c to cause deadlock

--- Code/Parallel/mpi/send2.c
+++ Code/Parallel/mpi/send2.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#define DATA_SIZE 10000
 int main(int argc, char **argv) {
   int procID, numP;
   MPI_Status status;
@@ -8,18 +9,19 @@ int main(int argc, char **argv) {
   MPI_Comm_size( MPI_COMM_WORLD, &numP );
   MPI_Comm_rank( MPI_COMM_WORLD, &procID );
 
+  int buf[DATA_SIZE];
   if (procID == 0) {
-    int buf[2] = {12345, 67890};
-    MPI_Send(&buf, 2, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    MPI_Recv(&buf, 2, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
-    printf("%d Received %d %d\n", procID, buf[0], buf[1]);
+    printf("Buffer Size: %d\n",DATA_SIZE);
+    for (int i=0; i<DATA_SIZE; i++) buf[i]=1+i;
+    MPI_Send(&buf, DATA_SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    MPI_Recv(&buf, DATA_SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+    printf("%d Received %d %d\n", procID, buf[0], buf[DATA_SIZE-1]);
   } else if (procID == 1) {
-    int buf[2] = {67890, 12345};
-    MPI_Send(&buf, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    MPI_Recv(&buf, 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-    printf("%d Received %d %d\n", procID, buf[0], buf[1]);
+    for (int i=0; i<DATA_SIZE; i++) buf[i]=DATA_SIZE-i;
+    MPI_Send(&buf, DATA_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Recv(&buf, DATA_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    printf("%d Received %d %d\n", procID, buf[0], buf[DATA_SIZE-1]);
   }
-
 
   MPI_Finalize();
   return 0;
